74e25cb8d1270d8c2b97fd53471c3ced2fcdbf09
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-20 23:21:52 -0600

    configure routes for resources :users, new and create

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,13 @@ Rails.application.routes.draw do
   end
 
   resources :about, only: [:index]
+
+  resources :users, only: [:create, :new]
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
