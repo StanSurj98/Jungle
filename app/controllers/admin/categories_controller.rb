d7c16e7b90a9c67420f4754869a3a92386b08ed5
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-20 21:15:07 -0600

    solve displaying category names and number of products in each category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,16 @@
 class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.order(id: :desc).all
+    # Contains the number of products of a certain category
+    @num_of_prods = []
+    @products_count = @categories.each do |category|
+      results = category.products.size
+      puts results
+      @num_of_prods << results
+    end
+
+    @result = @categories.zip(@num_of_prods)
+
   end
 
   def new
