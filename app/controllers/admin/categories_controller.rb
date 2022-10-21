576016722626587833dce640ddde52c8fce0054f
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-20 21:22:52 -0600

    refactor passing categories and its counts to the partial

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,14 +2,15 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.order(id: :desc).all
     # Contains the number of products of a certain category
-    @num_of_prods = []
-    @products_count = @categories.each do |category|
+    @products_count = []
+    @categories.each do |category|
+      # find the # of products belonging to a category with "collections" syntax
       results = category.products.size
-      puts results
-      @num_of_prods << results
+      # push to our array for counts
+      @products_count << results
     end
-
-    @result = @categories.zip(@num_of_prods)
+    # found .zip(), pairs up two arrays and their data?
+    @categories_and_counts = @categories.zip(@products_count)
 
   end
 
