7364f36b2cc19887b4ecd3a41cdf69875acb1cb8
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-24 21:37:29 -0600

    add validations for :names, :email, :pass & :pass_confirmation ; passes all tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, 
+            :last_name, 
+            :password, 
+            :password_confirmation, 
+            presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   describe "Validations" do 
-    context "signing up with password and password_confirmation fields" do
+    context "password and password_confirmation fields" do
 
-      it "creates a new user when both fields match" do
+      it "creates a new user if both fields match" do
         @user = User.new(
           "first_name" => "John", 
           "last_name" => "Doe", 
@@ -18,7 +18,7 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
 
-      it "raises an error when fields do not match" do
+      it "raises an error if fields do not match" do
         @user = User.new(
           "first_name" => "John", 
           "last_name" => "Doe", 
@@ -33,7 +33,7 @@ RSpec.describe User, type: :model do
 
     end
 
-    context "signing up with emails" do
+    context "signing up through email" do
       before do
         @user1 = User.new(
           "first_name" => "John", 
@@ -45,7 +45,7 @@ RSpec.describe User, type: :model do
         @user1.save
       end
 
-      it "raises error if email is not unique" do
+      it "raises an error if email is not unique" do
         @user2 = User.new(
           "first_name" => "John", 
           "last_name" => "Doe", 
@@ -57,6 +57,101 @@ RSpec.describe User, type: :model do
 
         expect(@user2).to be_invalid
       end
+    end
+
+    context "filling relevant fields" do
+
+      it "saves successfully if all fields present" do
+        @user = User.new(
+          "first_name" => "Jane", 
+          "last_name" => "Doe", 
+          "email" => "jane_doe@gmail.com", 
+          "password" => "password", 
+          "password_confirmation" => "password"
+        )
+        @user.save
+
+        expect(@user.errors.full_messages).not_to be_present
+      end
+
+      it "raises :first_name error if First Name is missing" do
+        @user = User.new(
+          "first_name" => nil, 
+          "last_name" => "Doe", 
+          "email" => "jane_doe@gmail.com", 
+          "password" => "password", 
+          "password_confirmation" => "password"
+        )
+        @user.save
+
+        expect(@user.errors.full_messages).to be_present
+      end
+      
+      it "raises :first_name error if First Name is missing" do
+        @user = User.new(
+          "first_name" => nil, 
+          "last_name" => "Doe", 
+          "email" => "jane_doe@gmail.com", 
+          "password" => "password", 
+          "password_confirmation" => "password"
+        )
+        @user.save
+
+        expect(@user.errors.full_messages).to be_present
+      end
+
+      it "raises :last_name error if Last Name is missing" do
+        @user = User.new(
+          "first_name" => "Jane", 
+          "last_name" => nil, 
+          "email" => "jane_doe@gmail.com", 
+          "password" => "password", 
+          "password_confirmation" => "password"
+        )
+        @user.save
+
+        expect(@user.errors.full_messages).to be_present
+      end
+
+      it "raises :email error if Email is missing" do
+        @user = User.new(
+          "first_name" => "Jane", 
+          "last_name" => "Doe", 
+          "email" => nil, 
+          "password" => "password", 
+          "password_confirmation" => "password"
+        )
+        @user.save
+
+        expect(@user.errors.full_messages).to be_present
+      end
+
+      it "raises :password error if Password is missing" do
+        @user = User.new(
+          "first_name" => "Jane", 
+          "last_name" => "Doe", 
+          "email" => "jane_doe@gmail.com", 
+          "password" => nil, 
+          "password_confirmation" => "password"
+        )
+        @user.save
+
+        expect(@user.errors.full_messages).to be_present
+      end
+
+      it "raises :password_confirmation error if Password Confirmation is missing" do
+        @user = User.new(
+          "first_name" => "Jane", 
+          "last_name" => "Doe", 
+          "email" => "jane_doe@gmail.com", 
+          "password" => "password", 
+          "password_confirmation" => nil
+        )
+        @user.save
+
+        expect(@user.errors.full_messages).to be_present
+      end
+
     end
 
 
